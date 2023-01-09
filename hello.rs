aa29d2005731603c965ac7bef7548213040bc155
Gagan Nayyar <gagan.nayyar@outlook.com>
2023-01-09 17:13:04 +0000

    added some rust

--- hello.rs
+++ hello.rs
@@ -1,9 +1,26 @@
 fn main() {
+    let user1 = User{
+        active: true,
+        username: "Someone".to_string(),
+        sign_in_count: 0
+    };
+    println!("{}",user1.username);
 
-    let x = vec!["tyler".to_string()];
-    let y = x;
-    let z = y;
-    println!("{:?}",z);
+    let user2 = build_user(String::from("SomeoneElse"));
+    println!("{}",user2.username)
+}
 
+struct User {
+    active: bool,
+    username: String,
+    sign_in_count: u32 
+}
+
+fn build_user(username: String) -> User {
+    User{
+        username,
+        active: true,
+        sign_in_count: 1
+    }
 }
 
